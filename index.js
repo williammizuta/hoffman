c6a94186e6237d104024a469797f29b4f6291f4c
diffsky <matthew.hadley@gmail.com>
2013-03-24 22:25:17 -0700

    update tests - use istanbul for code coverage - add jshint rules - add grunt to run tasks

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-var fs = require('fs')
-  , path = require('path')
-  , duster = require('duster')
-  , dust = duster.dust
-  , views;
+"use strict";
+
+var fs = require('fs'),
+    path = require('path'),
+    duster = require('duster'),
+    dust = duster.dust,
+    views;
 
 dust.onLoad = function (template, cb) {
   // if no extname then handling a partial, figure out the full path
-  if (path.extname(template) == '') {
+  if (path.extname(template) === '') {
     template = path.join(views, template) + '.dust';
   }
   // read the template off disk
   fs.readFile(template, 'utf8', function(err, data){
-    if (err) return cb(err);
+    if (err) {
+      return cb(err);
+    }
     cb(err, data);
-  })
-}
+  });
+};
 
 function clearCacheCheck(clear){
   if (clear === undefined || clear === false) {
@@ -25,18 +29,24 @@ function clearCacheCheck(clear){
 module.exports = {
   __express: function() {
     return function(template, options, cb){
-      if (!views) views = options.settings.views;
+      if (!views) {
+        views = options.settings.views;
+      }
       template = path.relative(views, template).slice(0, -5);
       dust.render(template, options, function(err, output){
-        if (err) return cb(err);
+        if (err) {
+          return cb(err);
+        }
         clearCacheCheck(options.settings["view cache"]);
         cb(err, output);
       });
-    }
+    };
   },
   stream: function(req, res, next) {
     res.stream = function(template, data, cb){
-      if (!views) views = req.app.settings.views;
+      if (!views) {
+        views = req.app.settings.views;
+      }
       var stream = dust.stream(template, data);
       if (cb) {
         cb(stream);
@@ -46,7 +56,7 @@ module.exports = {
             res.write(chunk);
           }
         })
-        .on('end', function(err) {
+        .on('end', function() {
           res.end();
           clearCacheCheck(req.app.settings["view cache"]);
         })
--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
-var assert = require('chai').assert
-  , fs = require('fs')
+var assert = require('chai').assert,
+    path = require('path');
 
 var options = {
   "settings" : {
@@ -11,69 +11,100 @@ var options = {
 
 var hoffman = require('../index.js');
 var render = hoffman.__express();
-var templates = __dirname + '/templates';
-var data = {"planet" : "world"};
+var views = __dirname + '/templates';
+var templates = {
+  "hello" : views + '/hello.dust',
+  "main" : views + '/main.dust',
+  "invalid" : ""
+};
 
-
+// when a new hoffman instance is required
+function newHoffman() {
+  delete require.cache[path.join(__dirname, '../index.js')];
+  hoffman = require('../index.js');
+  render = hoffman.__express();
+}
 
 describe('Dust object', function(){
   it('should be available', function(){
     assert.typeOf(hoffman.dust, 'object', 'hoffman.dust is an object');
-  })
+  });
+});
+
+describe('Dust templates', function(){
+  it('should be loaded from disk', function(done){
+    hoffman.dust.onLoad(templates.hello, function(err, str){
+      assert.equal(str, 'hello {planet}', 'hello.dust templates is hello {planet}');
+      done();
+    });
+  });
+  it('should throw an error if cannot be loaded', function(done){
+    hoffman.dust.onLoad(templates.invalid, function(err){
+      assert.typeOf(err, 'object', 'err contains an error');
+      done();
+    });
+  });
 });
 
 describe('Dust templates', function(){
   it('should be parsed into output', function(done){
-    render(templates + '/hello.dust', options, function(err, output){
-      assert.equal( "hello world", output);
+    render(templates.hello, options, function(err, output){
+      assert.equal("hello world", output);
       done();
-    })
-  })
+    });
+  });
   it('should dynamically include partials', function(done){
-    render(templates + '/main.dust', options, function(err, output){
-      assert.equal( "hello world", output);
+    render(templates.main, options, function(err, output){
+      assert.equal("hello world", output);
       done();
-    })
-  })
+    });
+  });
+  it('should throw an error if fail to render', function(done){
+    render(templates.invalid, options, function(err){
+      assert.typeOf(err, 'object', 'err contains an error');
+      done();
+    });
+  });
 });
 
 describe('Dust cache', function(){
   it('should not store rendered templates when disabled', function(done){
-    render(templates + '/hello.dust', options, function(err, output){
-      render(templates + '/main.dust', options, function(err, output){
+    render(templates.hello, options, function(){
+      render(templates.main, options, function(){
         assert.typeOf(hoffman.dust.cache.hello, 'undefined', 'dust.cache.hello is undefined');
         done();
-      })
-    })
-  })
+      });
+    });
+  });
 
   it('should store rendered templates when enabled', function(done){
     options.settings["view cache"] = true;
-    render(templates + '/hello.dust', options, function(err, output){
-      render(templates + '/main.dust', options, function(err, output){
+    render(templates.hello, options, function(){
+      render(templates.main, options, function(){
         assert.typeOf(hoffman.dust.cache.hello, 'function', 'dust.cache.hello is a function');
         done();
-      })
-    })
-  })
+      });
+    });
+  });
   it('should be primable with all templates in the view dir', function(done){
     hoffman.prime(options.settings.views);
     assert.lengthOf(Object.keys(hoffman.dust.cache), 3, 'dust.cache contains 3 functions');
     done();
-  })
+  });
 });
 
 describe('Dust streaming', function(){
-  it('should be supported', function(done){
-    var output = '';
-    var req = { "app" : options };
+  it('should be supported via returning a stream instance', function(done){
+    newHoffman();
+    var output = '',
+        req = { "app" : options };
     hoffman.stream(req, {}, function(err, req, res){
-      res.stream('hello', options, function(stream){
+      res.stream(templates.hello, options, function(stream){
         stream.on('data', function(chunk) {
           output = output + chunk;
         })
-        .on('end', function(err) {
-          assert.equal( "hello world", output);
+        .on('end', function() {
+          assert.equal("hello world", output);
           done();
         })
         .on('error', function(err) {
@@ -83,8 +114,19 @@ describe('Dust streaming', function(){
         });
       });
     });
-  })
+  });
+
+  it('should be supported via internally streaming to res.write', function(done){
+    var req = { "app" : options },
+        res = {
+      "write" : function(chunk){
+        assert.equal("hello world", chunk);
+      },
+      "end" : function(){}
+    };
+    hoffman.stream(req, res, function(err, req, res){
+      res.stream(templates.hello, options);
+      done();
+    });
+  });
 });
-
-
-
